02cb537be16b45fdb0c1fb32cc8886f5cc313e35
justinsa <justinsa@live.ca>
2013-09-27 12:29:25 -0700

    Making progress on Angular implementation
    * Angular page does not throw any errors currently... which is positive.
    No graphs are being displayed, but that will come in the next submit.
    * Moved example data sets out to example-data.js so it can be shared
    across pages.

--- assets/scripts/angular-application.js
+++ assets/scripts/angular-application.js
@@ -2,3 +2,4 @@
 var application = angular.module('angular-card-ui-visualizations', ['card-ui-visualizations', 'ui.bootstrap']);
 application.controller('PageController', ['$scope', function ($scope) {}]);
 application.run([function () {}]);
+angular.bootstrap(document, ['angular-card-ui-visualizations']);
