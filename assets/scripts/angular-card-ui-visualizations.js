795dd32026a3478d87a4b620cd40e37520c799ad
justinsa <justinsa@live.ca>
2013-09-27 17:24:11 -0700

    Basic parts of the angular directives in place
    * Nothing special here, just some setup.

--- assets/scripts/angular-card-ui-visualizations.js
+++ assets/scripts/angular-card-ui-visualizations.js
@@ -4,19 +4,58 @@ directives.directive('cardBlock', function() {
   return {
     restrict: 'E',
     replace: true,
+    scope: {
+      'addClickCallback': '&',
+      'addCount': '=?',
+      'rowFill': '=?',
+      'rowSize': '=?',
+      'stack': '=?',
+      'stackCardOffset': '=?',
+      'stackDescend': '=?',
+      'dataSet': '&set'
+    },
     transclude: false,
     templateUrl: 'partials/card-block-directive.html',
-    link: function (scope, element, attributes) {},
-    controller: ['$scope', function ($scope) {}]
+    link: function (scope, element, attributes) {
+      _.defaults(
+        scope,
+        {
+          'addClickCallback': _.identity,
+          'addCount': 1,
+          'rowSize': 10,
+          'stack': false,
+          'stackCardOffset': 0,
+          'stackDescend': false
+        }
+      );
+    }
   };
 });
 directives.directive('cardRing', function() {
   return {
     restrict: 'E',
     replace: true,
+    scope: {
+      'addClickCallback': '&',
+      'addCount': '=?',
+      'clockwise': '=?',
+      'ringFill': '=?',
+      'ringSize': '=?',
+      'dataSet': '&set'
+    },
     transclude: false,
     templateUrl: 'partials/card-ring-directive.html',
-    link: function (scope, element, attributes) {},
-    controller: ['$scope', function ($scope) {}]
+    link: function (scope, element, attributes) {
+      _.defaults(
+        scope,
+        {
+          'addClickCallback': _.identity,
+          'addCount': 1,
+          'clockwise': false,
+          'ringFill': false,
+          'ringSize': 0
+        }
+      );
+    }
   };
 });
