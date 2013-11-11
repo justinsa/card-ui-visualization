24c2043f86347ece9631530708b22022fd38c6fc
justinsa <justinsa@live.ca>
2013-11-11 15:55:24 -0800

    Remove Angular and Ember from project

--- web.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var PORT = typeof(process.env.PORT) !== 'undefined' ? process.env.PORT : 8080;
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname));
-app.get('/?', function(request, response) {
-  response.sendfile(__dirname + '/index.html');
-});
-app.listen(PORT);
