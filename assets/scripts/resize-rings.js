56d59e8c140a1688a4e769760d2cc35ced6bcb08
Noah Kaplan <noahkaplan4@gmail.com>
2014-04-24 11:05:19 -0700

    added code to remove old cards when rings are resized

--- assets/scripts/resize-rings.js
+++ assets/scripts/resize-rings.js
@@ -1,6 +1,12 @@
 $( document ).ready(function() {
 
   function resizeRings(width, height){
+    var cardRingDivs = document.getElementsByClassName('card-ring');
+    _.each(cardRingDivs, function(div){
+      while (div.hasChildNodes()) {
+        div.removeChild(div.lastChild);
+      }
+    })
     $('.card-ring').css({'height': height, 'width': width});
     $('.card-ring').each(function() { window['card-ui-visualizations']['card-ring'](this); });
   }
